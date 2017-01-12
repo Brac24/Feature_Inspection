2e51bcd2521e3953da4ed2eb2f2ae7fce6f7fbc7
Brac24 <2brac4@gmail.com>
2017-01-12 10:36:45 -0800

    nothing

--- AlternateUI.cs
+++ AlternateUI.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using System.Data.Odbc;
 using BrightIdeasSoftware;
 /*ALL TABLES REFER TO THE ATI_FeatureInspection Database */
-//THIS IS NOT THE MASTER
+//THIS IS THE similarWithDifferentDataTables BRANCH
 namespace Feature_Inspection
 {
 
